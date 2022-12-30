b4f559f9ad11c0ff6fac98113412b3b2a64e9f89
Ricardo Nunes <up202109480@edu.fe.up.pt>
2022-12-30 11:41:28 +0000

    Add player alive pieces

--- tp3/objects/Player.js
+++ tp3/objects/Player.js
@@ -9,9 +9,19 @@ export class Player{
         this.scene = scene;
         this.isPlayerTurn = false;
         this.id = id;
+		this.alivePieces = [];
     }
 
     setIsPlayerTurn(value){
         this.isPlayerTurn = value;
+    }
+	
+	addPieceAlivePieces(piece){
+        this.alivePieces.push(piece)
+    }
+	
+	removePieceAlivePieces(piece){
+		var index = this.alivePieces.findIndex(piece);
+        this.alivePieces.slice(piece, 1);
     }
 }
